f9dbab668baa2ab35b512004f77e958fe67315ae
Mummakill <mykhailo.sukhorukov@n-cube.co.uk>
2017-12-12 07:52:48 +0000

    configs from job

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+#
+# ~/.bash_profile
+#
+if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
+	exec startx
+fi
---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -9 -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar > /dev/null; do sleep 1; done
+
+if [ $(cat /sys/class/drm/card0-HDMI-A-1/status) == "connected" ] ; then
+    polybar top &
+    polybar bottom &
+    polybar external_bottom &
+else
+    polybar top &
+    polybar bottom &
+fi
